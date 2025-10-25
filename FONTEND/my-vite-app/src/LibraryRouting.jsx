052323b3c06cb5f2f6eb341245b9650c0fb851db
shyam <shyammegha493@gmail.com>
2025-10-25 18:37:34 +0530

    commited frontend folder

--- FONTEND/my-vite-app/src/LibraryRouting.jsx
+++ FONTEND/my-vite-app/src/LibraryRouting.jsx
@@ -15,11 +15,6 @@ const LibraryRouting = () => {
     <nav class="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-400	flex flex-wrap items-center text-base justify-center">
     <button type="button" class="text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 shadow-lg shadow-green-500/50 dark:shadow-lg dark:shadow-green-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"><Link to="/HomeLibrary">Home</Link></button>
     </nav>
-    <button class="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">Button
-      <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-1" viewBox="0 0 24 24">
-        <path d="M5 12h14M12 5l7 7-7 7"></path>
-      </svg>
-    </button>
   </div>
 </header>
     </div>
