b888dcfd65ebeeae664375088a720ff60885d6f9
shyam <shyammegha493@gmail.com>
2025-10-30 21:06:12 +0530

    all folder are commited

--- FONTEND/my-vite-app/src/CALCULATOR/CalculatorPage.jsx
+++ FONTEND/my-vite-app/src/CALCULATOR/CalculatorPage.jsx
@@ -1,11 +1,41 @@
 import React from 'react'
 import api from '../api/axios'
+
 const CalculatorPage = () => {
+  const buttons = ["1","2","3","4","5","6","7","8","9","0","=","X","AC"];
+
   return (
-    <div>
+    <div style={styles.container}>
       <h1>This is standard page</h1>
+
+      <div style={styles.grid}>
+        {buttons.map((btn) => (
+          <button key={btn} style={styles.btn}>{btn}</button>
+        ))}
+      </div>
     </div>
   )
+}
+
+const styles = {
+  container: { textAlign: "center", padding: "20px" },
+
+  grid: {
+    display: "grid",
+    gridTemplateColumns: "repeat(3, 90px)",
+    gap: "12px",
+    justifyContent: "center",
+    marginTop: "20px",
+  },
+
+  btn: {
+    padding: "18px",
+    borderRadius: "10px",
+    fontSize: "22px",
+    cursor: "pointer",
+    border: "1px solid #333",
+    background: "#f8f9fa"
+  }
 }
 
 export default CalculatorPage
