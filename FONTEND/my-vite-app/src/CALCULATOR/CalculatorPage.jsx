10c16b085d77797bc6cf959652ed30a67c969717
shyam <shyammegha493@gmail.com>
2025-10-30 21:03:05 +0530

    all folder are commited

---
+++ FONTEND/my-vite-app/src/CALCULATOR/CalculatorPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import api from '../api/axios'
+const CalculatorPage = () => {
+  return (
+    <div>
+      <h1>This is standard page</h1>
+    </div>
+  )
+}
+
+export default CalculatorPage
